e517ae7538beb6fff782f7ea9bfd057a11e522c9
Lorhana <lorhana_martins@hotmail.com>
2022-06-28 20:45:44 -0300

    aula js 06

---
+++ 06/index.js
@@ -0,0 +1,28 @@
+function carregar(){
+    /* MODAL */
+
+    document.getElementsByClassName('modal-container')[0].style.display = 'block'
+}
+
+
+
+
+
+function menu(){
+   // Verifica Checkbox
+   var check = document.getElementsByName('checkMenu')[0].checked 
+   console.log(check)
+   
+   if(check){
+    document.getElementsByClassName('container-menu')[0].style.display = "block" 
+
+   }else{
+    document.getElementsByClassName('container-menu')[0].style.display = "none" 
+   }
+
+}
+
+function fecharModal(){
+    /* MODAL */
+    document.getElementsByClassName('modal-container')[0].style.display = 'none'
+}
